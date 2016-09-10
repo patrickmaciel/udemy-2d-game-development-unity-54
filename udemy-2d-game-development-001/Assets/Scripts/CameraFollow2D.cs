1ccd30e1b2016dd0067df2561c3a5ae7c7cdefa0
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-09-10 10:52:53 -0300

    26. Create a camera node with constraints

---
+++ udemy-2d-game-development-001/Assets/Scripts/CameraFollow2D.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow2D : MonoBehaviour {
+
+    public float boundsTop = 0.0f;
+    public float boundsBottom = 0.0f;
+    public float boundsLeft = 0.0f;
+    public float boundsRight = 0.0f;
+
+    private Transform target;
+
+	// Use this for initialization
+	void Start () {
+        target = GameObject.FindWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(
+                                Mathf.Clamp(target.position.x, boundsLeft, boundsRight),
+                                Mathf.Clamp(target.position.y, boundsBottom, boundsTop),
+                                transform.position.z
+        );
+	}
+}
