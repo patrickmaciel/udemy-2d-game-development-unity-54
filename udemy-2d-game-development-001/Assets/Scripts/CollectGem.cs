2f829cf3744b548c0f913de1d37747c9885a48ea
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-09-10 10:02:26 -0300

    1-22. Create a Collectible Item for the Character to pick up

---
+++ udemy-2d-game-development-001/Assets/Scripts/CollectGem.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectGem : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        // Tag of the GameObject
+        if (target.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
