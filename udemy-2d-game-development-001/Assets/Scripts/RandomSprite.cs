a525b4f8edc77ff84ce27acaea8ebf07ce5ca5f6
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-09-10 10:23:30 -0300

    24. Randomized Scenery

---
+++ udemy-2d-game-development-001/Assets/Scripts/RandomSprite.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomSprite : MonoBehaviour {
+
+    public Sprite[] sprites;
+
+	// Use this for initialization
+	void Start () {
+        var myRenderer = GetComponent<SpriteRenderer>();
+        if (sprites.Length > 0)
+        {
+            myRenderer.sprite = sprites[Random.Range(0, sprites.Length)];
+        }	
+	}
+}
