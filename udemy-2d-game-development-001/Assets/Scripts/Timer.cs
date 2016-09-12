3a652c3035f18fb41801d71904ab10222f51772f
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-09-12 20:59:02 -0300

    34. Create a timer and display it on screen

---
+++ udemy-2d-game-development-001/Assets/Scripts/Timer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public float valueRemaining = 10;
+    public float valueMaximum = 30;
+    public float valueConsumptionRate = 1f;
+
+    public Text timerText;
+
+    public bool gameInProgress = true;
+    
+	// Update is called once per frame
+	void Update () {
+	    if (gameInProgress)
+        {
+            valueRemaining -= Time.deltaTime * valueConsumptionRate;
+            timerText.text = "Tempo restante: " + Mathf.Round(valueRemaining).ToString();
+
+            if (valueRemaining < 0)
+            {
+                gameInProgress = false;
+                endGameWithFailure();
+            }
+            // TODO
+        }
+	}
+
+    void endGameWithFailure()
+    {
+
+    }
+}
