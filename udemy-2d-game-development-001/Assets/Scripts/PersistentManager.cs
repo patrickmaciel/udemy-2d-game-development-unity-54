6a8e05a8e8db2ac38f62489fcafb3770982dbfc0
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-09-11 23:03:35 -0300

    31. Saving and loading game data

--- udemy-2d-game-development-001/Assets/Scripts/PersistentManager.cs
+++ udemy-2d-game-development-001/Assets/Scripts/PersistentManager.cs
@@ -7,34 +7,70 @@ using UnityEngine.SceneManagement;
 
 public class PersistentManager : MonoBehaviour {
 
-    public static PersistentManager dataStore;
+	public static PersistentManager dataStore;
 
-    // Instance variable
-    public int currentLevelID;
+	// Instance variable
+	public int currentLevelID;
 
-    // Persistent Variables
-    public int gemsCollected;
-    public int highestLevelCompleted;
+	// Persistent Variables
+	public int gemsCollected;
+	public int highestLevelCompleted;
 
-    void Awake()
-    {
-        if (dataStore == null)
-        {
-            DontDestroyOnLoad(gameObject);
-            dataStore = this;
-        } else if (dataStore != this)
-        {
-            Destroy(gameObject);
-        }
-    }
+	void Awake()
+	{
+		if (dataStore == null)
+		{
+			DontDestroyOnLoad(gameObject);
+			dataStore = this;
+            Load();
+		} else if (dataStore != this)
+		{
+			Destroy(gameObject);
+		}
+	}
 
 	// Use this for initialization
 	void Start () {
-	    
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
+
+    // Saving and loading
+    public void Save()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/GameData.dat");
+
+        GameData data = new GameData();
+        data.gemsCollectedTotal = gemsCollected;
+        data.highestLevel = highestLevelCompleted;
+
+        bf.Serialize(file, data);
+        file.Close();
+    }
+
+    public void Load()
+    {
+        if (File.Exists(Application.persistentDataPath + "/GameData.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/GameData.dat", FileMode.Open);
+            GameData data = (GameData)bf.Deserialize(file);
+            file.Close();
+
+            gemsCollected = data.gemsCollectedTotal;
+            highestLevelCompleted = data.highestLevel;
+        }
+    }
+}
+
+[Serializable]
+class GameData
+{
+	public int gemsCollectedTotal;
+	public int highestLevel;
 }
